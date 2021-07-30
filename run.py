0ddd69e13111f5f60db67f78b22bc4ea8a23a79b
dingmeng <souldmly@163.com>
2021-07-30 13:59:53 +0800

    更新代码

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import sys
-sys.path.append(r"E:\自动化\new_enovate_interface_auto")
-
 import os
 import pytest
-import zmail
 
 
 if __name__ == '__main__':
@@ -13,10 +9,10 @@ if __name__ == '__main__':
     os.system('allure generate --clean ./report/temp -o ./report/report')
 
     # pytest.main()
-    mail_content = {
-        "subject": "哈哈",
-        "content_text": "测试测试,测试报告链接为\nhttp://localhost:63342/new_test_enovate_interface/report/report/index.html",
-        # "attachments": r"E:\自动化\new_enovate_interface_auto\report\report\index.html"  # 邮件附件（路径）
-    }
-    server = zmail.server("souldmly@163.com", "VREUQQHJPYSFWQDY")
-    server.send_mail("1356798645@qq.com", mail_content)
+    # mail_content = {
+    #     "subject": "哈哈",
+    #     "content_text": "测试测试,测试报告链接为\nhttp://localhost:63342/new_test_enovate_interface/report/report/index.html",
+    #     # "attachments": r"E:\自动化\new_enovate_interface_auto\report\report\index.html"  # 邮件附件（路径）
+    # }
+    # server = zmail.server("souldmly@163.com", "VREUQQHJPYSFWQDY")
+    # server.send_mail("1356798645@qq.com", mail_content)
