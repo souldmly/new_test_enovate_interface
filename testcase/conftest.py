1d098c376734d029f0f4c72e2cd7f8b095732e05
dingmeng <souldmly@163.com>
2021-09-07 16:02:06 +0800

    更新代码

--- testcase/conftest.py
+++ testcase/conftest.py
@@ -10,16 +10,16 @@ def env_config(request):
     """
     读取yml配置文件
     """
-    project_name = 'new_enovate_interface_auto'
-    rootPath = get_root_path(project_name)
+    # project_name = 'new_enovate_interface_auto'
+    rootPath = r'E:\自动化\new_enovate_interface_auto'
     config_path = os.path.abspath(rootPath + r'\config\\env_config.yml')  # 获取tran.csv文件的路径
     with open(config_path) as f:
         env_config = yaml.load(f)  # 读取配置文件
     return env_config
 
 
-def get_root_path(project_name):
-    curPath = os.path.abspath(os.path.dirname(__file__))
-    # rootPath = curPath[:curPath.find(project_name+"\\") + len(project_name+"\\")]  # 获取myProject，也就是项目的根路径
-    rootPath = r'E:\自动化\new_enovate_interface_auto'
-    return rootPath
+# def get_root_path(project_name):
+#     curPath = os.path.abspath(os.path.dirname(__file__))
+#     # rootPath = curPath[:curPath.find(project_name+"\\") + len(project_name+"\\")]  # 获取myProject，也就是项目的根路径
+#     rootPath = r'E:\自动化\new_enovate_interface_auto'
+#     return rootPath
